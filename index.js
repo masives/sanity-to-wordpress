c1f9a79d4a4eed75d26891193626a40518e7e3b7
Krzysztof Fraus <krz.fraus@gmail.com>
2020-08-07 09:09:20 +0200

    Suggested fixes

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const { Command } = require('commander');
 const { getWordpressData } = require('./lib/resources');
 const { saveDataAsNdJson } = require('./lib/ndjson');
 
-const startsWithHttpRegex = /^https?:\/\//;
 const program = new Command();
 
+// FIXME - I would add some "--help" option with the basic description
+// FIXME - there is inconsistency in names between line 10 i 13.
+// FIXME - In the line below I would extend the description somehow: What is base URL and what the "endpoint" means. Also add some example.
 program.option('-u, --url <api-url>', 'base url without endpoint ').parse(process.argv);
 
 if (!program.url) {
-  console.error('Please provide --url <wordpress-site-url>');
+  console.error('Please provide correct "--url" option. <wordpress-site-url>');
   process.exit(1);
 }
-
-if (!startsWithHttpRegex.test(program.url)) {
-  console.error('Url has to start with protocol http:// or https://');
-  process.exit(1);
-}
-
 const baseUrl = program.url;
 
 (async () => {
